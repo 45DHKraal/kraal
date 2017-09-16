453b9f1a69ddf5beea1700684e6cd29dc8218edd
Bartosz Markowski <b.markowski@getstrong.be>
2017-09-16 19:58:36 +0200

    Fix formatter

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,8 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 {:ok, _} = Application.ensure_all_started(:faker_elixir_octopus)
 
-if System.get_env("CI") do
-  ExUnit.configure formatters: [JUnitFormatter]
+if System.get_env("CI") == true do
+  ExUnit.configure formatters: [JUnitFormatter, ExUnit.CLIFormatter]
 end
 ExUnit.start()
 
